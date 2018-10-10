dc4c6ae2b98627a875989192bb040250867b14a5
marcinpiegza <marcinpiegza.mp@gmail.com>
2018-10-10 19:06:21 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/pl/project/calculator/controller/ExchangeController.java
    #	src/main/java/pl/project/calculator/controller/ExchangeViewController.java
    #	src/main/java/pl/project/calculator/model/ExchangeResult.java

--- src/main/java/pl/project/calculator/controller/ExchangeViewController.java
+++ src/main/java/pl/project/calculator/controller/ExchangeViewController.java
@@ -1,4 +1,13 @@
 package pl.project.calculator.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ExchangeViewController {
+
+    @GetMapping("/")
+    public String displayMainView(){
+        return "index.html";
+    }
 }
