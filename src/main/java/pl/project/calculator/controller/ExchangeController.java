0d3f26106c9e2e121b826c90c3b646d7f4f787ef
unknown <marcinpiegza.mp@gmail.com>
2018-10-10 18:00:45 +0200

    UpdateClass

--- src/main/java/pl/project/calculator/controller/ExchangeController.java
+++ src/main/java/pl/project/calculator/controller/ExchangeController.java
@@ -1,4 +1,19 @@
 package pl.project.calculator.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ExchangeController {
+
+    //przy parzowaniu daty musimy springowi powiedziec o formacie ISO  czyli napisac iso= DateTimeFormat.ISO.DATE
+
+    // albo wysyłąmy wszystkie parametry @PathVariable lub robimy jeden obiekt @RequestBody MyObject object
+    //wiec do musi to sami przemyslec
+
+
+    @GetMapping("/exchange/(value}/{date}"){
+         return new ResponseEntity- w taki obiekt mamy opanowac finalny wynik wraz ze statusem httpStatus
+     //bo to jest fajny wraper do opanowywania restowych wyników
+   }
+
 }
--- src/main/java/pl/project/calculator/model/ExchangeResult.java
+++ src/main/java/pl/project/calculator/model/ExchangeResult.java
@@ -1,4 +1,51 @@
 package pl.project.calculator.model;
 
+import org.springframework.http.HttpStatus;
+
+import java.math.BigDecimal;
+
 public class ExchangeResult {
+
+    private BigDecimal money;
+    private String error;
+    private HttpStatus status;
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public ExchangeResult(BigDecimal money, String error, HttpStatus status) {
+        this.money = money;
+        this.error = error;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeResult{" +
+                "money=" + money +
+                ", error='" + error + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
