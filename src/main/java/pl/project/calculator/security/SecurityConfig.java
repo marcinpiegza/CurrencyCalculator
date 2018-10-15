25123ed1e6f123395a2e91242ea6b19e9147e53a
Rob <balonekrobert@gmail.com>
2018-10-15 23:07:22 +0200

    małe zmiany x2

--- src/main/java/pl/project/calculator/security/SecurityConfig.java
+++ src/main/java/pl/project/calculator/security/SecurityConfig.java
@@ -1,50 +1,40 @@
 package pl.project.calculator.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.sql.DataSource;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    private final DataSource dataSource;
-
-    public SecurityConfig(DataSource dataSource){
-        this.dataSource = dataSource;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("user")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-     auth
-     .jdbcAuthentication()
-     .dataSource(dataSource)
-     .withUser("admin")
-     .password("{noop}admin")
-             .roles("USER");
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeRequests()
+                .antMatchers("/exchange").permitAll()
+                .antMatchers("/admin**").hasRole("ADMIN")
+                .and().httpBasic();
+
+        httpSecurity.csrf().disable();
     }
 
-
-
-
-
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/register").permitAll()
-//                .antMatchers("/authorization/save").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll();
-//    }
-
-
-
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
